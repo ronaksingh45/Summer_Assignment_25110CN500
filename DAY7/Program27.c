ad63080bfa9b80e4b74151f4170e9a34a9459fec
Ronak <singhronak13639@gmail.com>
2026-06-03 10:43:54 +0530

    DAY7 uploaded.

---
+++ DAY7/Program27.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sumDigits(int n)
+{
+    if(n==0)
+        return 0;   
+    return n%10 + sumDigits(n/10);   // recursive call
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Sum of digits is: %d",sumDigits(n));
+    return 0;
+}
